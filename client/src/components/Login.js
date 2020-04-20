d4c6b2cb3dd3a65785f81d039a034b8db91f064f
Eric Bourne <ericbourne6371@gmail.com>
2020-04-20 16:25:10 -0700

    added #19 clientListView, also cleaned up login, got rid of forgot password href and remember me radio button

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -69,10 +69,10 @@ export default function SignIn() {
             id='password'
             autoComplete='current-password'
           />
-          <FormControlLabel
+          {/* <FormControlLabel
             control={<Checkbox value='remember' color='primary' />}
             label='Remember me'
-          />
+          /> */}
           <Button
             type='submit'
             fullWidth
@@ -82,18 +82,6 @@ export default function SignIn() {
           >
             Sign In
           </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href='#' variant='body2'>
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href='#' variant='body2'>
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
         </form>
       </div>
     </Container>
