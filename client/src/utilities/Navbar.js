e5ffe5385a8b3c0d4669d4513561cdaf1730a5d5
Eric Bourne <ericbourne6371@gmail.com>
2020-04-22 14:53:10 -0700

    #14 working on navigation, implementing material ui

--- client/src/utilities/Navbar.js
+++ client/src/utilities/Navbar.js
@@ -1,49 +1,7 @@
-import React, { Component } from "react";
 
-export default class Navbar extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isAuthorized: false,
-    };
-  }
-  componentWillMount() {
-    if (sessionStorage.getItem("isAuthorized") === "yes") {
-      this.setState({ isAuthorized: true });
-    }
-  }
-  render() {
-    return (
-      <header class='header'>
-        <a href='/' className='logo'>
-          Porton
-        </a>
-        <input class='menu-btn' type='checkbox' id='menu-btn' />
-        <label class='menu-icon' for='menu-btn'>
-          <span class='navicon' />
-        </label>
-        <ul class='menu'>
-          <li>
-            <a href='/'>Home</a>
-          </li>
-          {this.state.isAuthorized ? (
-            <ul></ul>
-          ) : (
-            <ul>
-              <li>
-                <a href='/login'>Login</a>
-                <br />
-                <a href='/appointmentlist'>Appointment List</a>
-                <br />
-                <a href='/cliniclist'>Clinic List</a>
-              </li>
-            </ul>
-          )}
-        </ul>
-      </header>
-    );
-  }
-}
+
+
+
 // return (
 //   <header class='header'>
 //     <a href='/' className='logo'>
