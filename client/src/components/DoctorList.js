7dd7710bfe74bbfb0e6bb7cf732f7c7c397fe5fd
Eric Bourne <ericbourne6371@gmail.com>
2020-04-26 11:45:13 -0700

    have a working list/add doctors

--- client/src/components/DoctorList.js
+++ client/src/components/DoctorList.js
@@ -26,22 +26,33 @@ export default function DoctorList() {
     var myHeaders = new Headers();
     myHeaders.append("Accept", "application/json");
     myHeaders.append("Content-Type", "application/json");
-    //  myHeaders.append(
-    //    "Cookie",
-    //    "EGG_SESS=DFXZpcM31bRAT37S2_AoV3e_rCJWbmBs42EshRLqz_HO84LJkupAfuZx03L0O3D-x9gHfxn3UplF38lobVXEn9VumLLoF9HFrygwTAWbk5P79ZigWk5ids1pRWM-QQQNvP5mTjYH1DXZ_8sEDDnyiN20qdPp_s51Z9tdU2MVJA0TD4K3ObejENNB9mUWMS6kkWHoxlbZVx57zX2q7crt1FYP3_XuHtrcPRpMtPNGepe_lklxZPZ-KPbxRmuzDDTV2Z1TwIhow48gkCg_tqNTa_RC55qRvtMRnt6GeUgGXU9vl-JflW-nmgNR1yrx_G4euxXwSWpS-K0vTEK_UqXGNgC7XQcPv8yGrjWbgZu9LgD5JiAqOmd4rObTewPMbgwf"
-    //  );
-
+    myHeaders.append("Access-Control-Allow-Origin", "*");
+    myHeaders.append(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    myHeaders.append(
+      "Cookie",
+      "EGG_SESS=DFXZpcM31bRAT37S2_AoV3e_rCJWbmBs42EshRLqz_HO84LJkupAfuZx03L0O3D-x9gHfxn3UplF38lobVXEn9VumLLoF9HFrygwTAWbk5P79ZigWk5ids1pRWM-QQQNvP5mTjYH1DXZ_8sEDDnyiN20qdPp_s51Z9tdU2MVJA0TD4K3ObejENNB9mUWMS6kkWHoxlbZVx57zX2q7crt1FYP3_XuHtrcPRpMtPNGepe_lklxZPZ-KPbxRmuzDDTV2Z1TwIhow48gkCg_tqNTa_RC55qRvtMRnt6GeUgGXU9vl-JflW-nmgNR1yrx_G4euxXwSWpS-K0vTEK_UqXGNgC7XQcPv8yGrjWbgZu9LgD5JiAqOmd4rObTewPMbgwf"
+    );
     var requestOptions = {
       method: "GET",
       headers: myHeaders,
 
       redirect: "follow",
     };
+    console.log("REQUEST:", requestOptions);
     fetch(BASE_URL + "/clinic/getdoctors", requestOptions)
       .then((response) => response.text())
       .then((result) => {
-        setListDoctors(result);
-        console.log(result);
+        const coolDoctor = JSON.parse(result);
+        console.log("cool variable", coolDoctor);
+        console.log(typeof coolDoctor);
+        if (coolDoctor.success) {
+          setListDoctors(coolDoctor.result); //this allows me to not throw an error when running
+        }
+        // setListDoctors(coolDoctor.result); //to properly JSON.parse, need to set var name to the result via having "variableName.result" set to the initial state
+        console.log(listDoctors);
       })
       .catch((error) => console.log("error", error));
   }
@@ -50,39 +61,40 @@ export default function DoctorList() {
     getDoctors();
   }, []);
 
-  return listDoctors.map((row) => {
-    return (
-      <div>
-        <TableContainer component={Paper}>
-          <Table
-            className={classes.table}
-            size='small'
-         aria-label='a dense table'
-          >
-            <TableHead>
-              <TableRow>
-                <TableCell>Doctor Name</TableCell>
-                <TableCell align='right'>Phone</TableCell>
-                <TableCell align='right'>Email</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {/* listDoctors.map((row) => ( */}
-              <TableRow key={row.doctorname}>
-                <TableCell component='th' scope='row'>
-                  {row.doctorname}
-                </TableCell>
-                <TableCell align='right'>{row.phone}</TableCell>
-                <TableCell align='right'>{row.email}</TableCell>
-              </TableRow>
-              {/* )) */}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <Button variant='contained' color='primary' href='./AddDoctor.js'>
-          Add Doctor
-        </Button>
-      </div>
-    );
-  });
+  return (
+    <div>
+      <TableContainer component={Paper}>
+        <Table
+          className={classes.table}
+          size='small'
+          aria-label='a dense table'
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell>Doctor Name</TableCell>
+              <TableCell align='right'>Phone</TableCell>
+              <TableCell align='right'>Email</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {listDoctors &&
+              listDoctors.map((row) => {
+                return (
+                  <TableRow key={row.doctorname}>
+                    <TableCell component='th' scope='row'>
+                      {row.doctorname}
+                    </TableCell>
+                    <TableCell align='right'>{row.phone}</TableCell>
+                    <TableCell align='right'>{row.email}</TableCell>
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Button variant='contained' color='primary' href='./AddDoctor.js'>
+        Add Doctor
+      </Button>
+    </div>
+  );
 }
