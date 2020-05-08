60cf98d552afc7ea11b1cad1677f833bbebe695e
Sebastian- <hmurgu@hotmail.com>
2020-05-08 09:19:54 -0700

    center text on 404 page

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -2,5 +2,9 @@ import React from "react";
 import { Typography } from "@material-ui/core";
 
 export default function PageNotFound(props) {
-  return <Typography variant="h3">Page Not Found</Typography>;
+  return (
+    <Typography variant="h3" align="center">
+      Page Not Found
+    </Typography>
+  );
 }
